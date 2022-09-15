366055c4afc1c32022c059e3bb3b5e7bda4acaa3
chelsaed <102196325+chelsaed@users.noreply.github.com>
2022-09-14 22:46:29 -0500

    user login

--- hsathlete-app/src/App.js
+++ hsathlete-app/src/App.js
@@ -30,6 +30,7 @@ function App() {
         
           <Route path='/HomePg' element={<HomePg />} />
           <Route path="/athletedetails/:id" element={ <Athletedetails/>}/>
+          
 
 
          
